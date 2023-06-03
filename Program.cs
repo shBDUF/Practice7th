4322317d9a8cf03a8eff7eb4919bd0143e7a8666
Grigoriy <inpuct@mail.ru>
2023-06-04 01:56:55 +0300

    1st Ex

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Введите размерность массива ");
+// Console.WriteLine("Количество строк -  ");
+// int lines = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Количество столбцов -  ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// double [,] array = new double[lines,columns];
+// Random rnd = new Random();
+// for (int i = 0; i < lines; i++)
+// {
+//     for (int j = 0; j < columns; j++)
+//     {
+//         array[i,j] = (rnd.NextDouble()*100); 
+//     }
+// }
+// for (int i = 0; i < lines; i++)
+// {
+//     for (int j = 0; j < columns; j++)
+//     {
+//         Console.Write("{0,3} ",(float)array[i,j]); 
+//     }
+//     Console.WriteLine();
+// }
