186b45f37bd4d3006a5f79cf44323a95968bd746
Grigoriy <inpuct@mail.ru>
2023-06-04 06:21:06 +0300

    HARD SORT

--- Program.cs
+++ Program.cs
@@ -20,3 +20,62 @@
 //     }
 //     Console.WriteLine();
 // }
+
+
+int columns = InputNumbers("Введите количество столбцов: ");
+int lines = InputNumbers("Введите количество строк: ");
+int range = InputNumbers("Введите диапозон от 1 до: ");
+int [,] Array = new int[lines,columns];
+CreateArray(Array);
+PrintArray(Array);
+SortArray(Array);
+PrintArray(Array);
+
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] > array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+int InputNumbers(string input)
+{
+    Console.WriteLine(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
